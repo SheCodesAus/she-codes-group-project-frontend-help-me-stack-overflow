71e28f0f7b06acd6a22ed8dd808f14d44d4b02b3
olivia zhao <68890723+oliviaosi@users.noreply.github.com>
2022-05-22 22:41:39 +0800

    update to 4 small cards a row

--- src/components/ReportCard/ReportCard.jsx
+++ src/components/ReportCard/ReportCard.jsx
@@ -26,12 +26,18 @@ function ReportCard(props) {
                   </div>
 
                   <div className="card2-wrapper">
-                    <div className="total">{reportData.total_attendance}</div>
-                    <div className="card-text-label">Total attendance</div>
+                    <div className="total">{reportData.transition_to_tech}</div>
+                    <div className="card-text-label">Transition to tech</div>
                     
                   </div>
 
                   <div className="card3-wrapper">
+                    <div className="total">{reportData.alumni_to_mentor}</div>
+                    <div className="card-text-label">Alumni to mentor</div>
+                    
+                  </div>
+
+                  <div className="card4-wrapper">
                     <div className="card-text-label">Coding languages:</div>
                     <div className="card-text-sml">{reportData.coding_languages}</div>
                   </div>
