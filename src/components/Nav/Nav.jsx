97f8cb0276742addab0b4967974d5e27b7cc040a
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-05-28 09:24:57 +0800

    Revert "Nav login/out enabled"
    This reverts commit e05cda5132bf409cdd5a40f6e625bd8207aa2518.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,40 +6,13 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
-
-    
-     // Hooks
-     const navigate = useNavigate();
-
-     // Actions and Helpers
-     const navigateToLogin = () => {
-         navigate("/login")
-     }
- 
-     const handleSignOut = () => {
-         window.localStorage.removeItem("token")
-         navigateToLogin()
-     }
- 
-     const checkUser = () => {
-         const isUserLoggedIn = !!window.localStorage.getItem("token");
-         // console.log("isUserLoggedIn", isUserLoggedIn)
- 
-         return isUserLoggedIn
-             ? <a href="logout" onClick={handleSignOut} className="login-btn">Log out</a>
-             : <a href="login" onClick={navigateToLogin} className="login-btn">Log in</a>
-     }
-
-
     return (
         <nav className="navbar">
             <div className="left-nav">
                 <Link to="/">Home</Link>
-                <Link to="/program">Program</Link>                
+                <Link to="/program">Program</Link>
             </div>
-            
-            <button className="login-btn">{checkUser()}</button>
-            
+            <button className="login-btn">Login</button>
         </nav>
     );
 }
