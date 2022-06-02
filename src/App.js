b1716c995cec23e1bdaba1a917f8eae32f7d9ac0
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-06-02 20:25:43 +0800

    Adding an Admindashboard

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Nav from "./components/Nav/Nav";
 //pages 
 import HomePage from "./pages/HomePage";
 import ProgramPage from "./pages/ProgramPage";
+import AdminPage from "./pages/AdminPages/AdminPage";
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />   
-          <Route path="/programs/:id" element={<ProgramPage />} />     
+          <Route path="/programs/:id" element={<ProgramPage />} />
+          <Route path="/admin" element={<AdminPage />}     />
         </Routes>
       </div>
     </Router>
