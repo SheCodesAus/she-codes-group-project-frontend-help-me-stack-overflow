9ad0140068a110c37ab25851007b9cacfc8e097f
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-06-02 23:28:32 +0800

    created and styled student form

---
+++ src/pages/StudentFormPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Components
+import StudentForm from "../components/StudentForm/StudentForm";
+
+function StudentFormPage() {
+    return <StudentForm />;
+}
+
+export default StudentFormPage;
